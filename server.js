928ae6bdb718b0066781e25a9238385496ad8f84
harshilchandratre <harshilchandratre2002@gmail.com>
2023-02-27 21:14:41 +0530

    calculator using server ver 1.0

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+
+const app = express()
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/index.html")
+})
+
+app.post("/", function(req, res){
+
+    var a = Number(req.body.n1);
+    var b = Number(req.body.n2);
+
+    var c = a + b; 
+
+    res.send("The result is " + c)
+})
+
+app.listen(3000, function(){
+    console.log("server is running successfully on 3000.")
+})
